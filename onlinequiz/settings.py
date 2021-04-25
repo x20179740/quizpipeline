c3506a7fa827b85e20b5a5724d3125f022e5fa14
EC2 Default User <ec2-user@ip-172-31-55-23.ec2.internal>
2021-04-25 14:21:51 +0000

    last commit

--- onlinequiz/settings.py
+++ onlinequiz/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '@k0#p3kidu)yaaa3u1hplxz)f@^6xiy384*(+n@@s5x#1bx@m5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0','54.226.13.36']
+ALLOWED_HOSTS = ['applicationname-env.eba-irenmz9y.us-east-1.elasticbeanstalk.com']
 
 
 # Application definition
