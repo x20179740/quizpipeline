126e2865dcf3078f438cc101af8305b29721d590
EC2 Default User <ec2-user@ip-172-31-55-23.ec2.internal>
2021-04-27 23:06:06 +0000

    django error

--- manage.py
+++ manage.py
@@ -2,7 +2,6 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from django.core.management import (execute_from_command_line)
 
 
 def main():
@@ -10,6 +9,7 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onlinequiz.settings')
     try:
         from django.core.management import execute_from_command_line
+   #plint:import-outside-toplevel
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
