c7d77cddb4cbca5118fe16453654b897c9f5e68a
EC2 Default User <ec2-user@ip-172-31-55-23.ec2.internal>
2021-04-29 17:12:09 +0000

    My last commit

--- manage.py
+++ manage.py
@@ -8,6 +8,8 @@ def main():
     '''this is missing docstring'''
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onlinequiz.settings')
     try:
+        #pylint: disable=wrong-import-position
+
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
